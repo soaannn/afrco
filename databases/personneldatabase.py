fa9a1b4a4dd41c2ae7578057b68b71fbf6aa5acb
soaannn <soanalbert44@gmail.com>
2025-08-22 22:56:20 +0200

    Create personneldatabase.py

---
+++ databases/personneldatabase.py
@@ -0,0 +1,44 @@
+{
+    "1228057908101648514": {
+        "roblox_username": "Studio_France",
+        "strikes": 0,
+        "notes": "Corporate Committee, Operations Department",
+        "flights_attended": 1.5
+    },
+    "1175161793103016009": {
+        "roblox_username": "Klomnarc",
+        "strikes": 0,
+        "notes": "Vice President"
+    },
+    "1147281809432920165": {
+        "roblox_username": "serag_liio",
+        "strikes": 0,
+        "notes": "Chief Executive Officer"
+    },
+    "1038740954221383681": {
+        "roblox_username": "samsonitebi",
+        "strikes": 0,
+        "notes": "Corporate Committee, Experience Department"
+    },
+    "1007581336590094407": {
+        "roblox_username": "AvGeekBG",
+        "strikes": 0,
+        "notes": "Secretarial Committee, Piloting Department"
+    },
+    "1204154413661552660": {
+        "roblox_username": "GlockQueenX",
+        "strikes": 0,
+        "notes": "President"
+    },
+    "1292118257565700251": {
+        "roblox_username": "realspvrtscar",
+        "strikes": 0,
+        "notes": "Corporate Committee, Human Resources Department"
+    },
+    "899654292598841415": {
+        "roblox_username": "N/A",
+        "flights_attended": 1.5,
+        "strikes": 0,
+        "notes": "N/A"
+    }
+}
